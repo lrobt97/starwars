26b01c85e0dee8a8a796e678f13fd622813dd015
lrobt97 <69146855+lrobt97@users.noreply.github.com>
2020-08-05 21:52:04 +0100

    Bootstrap, selector, cookie based page loading
    Added bootstrap to style the web page more efficiently. Also added in a select function with a maximum of 3 choices. The javascript saves these choices to a cookie, and the php script will load the page based on this cookie.

--- app/Views/charactersview.php
+++ app/Views/charactersview.php
@@ -1,7 +1,53 @@
-<?php $id = 1; ?>
+<?php 
+   $id = 1; 
+   $items;
+   if(!is_null($_COOKIE['items']))
+   {
+	   $items = json_decode($_COOKIE['items']);
+   }
+   echo "<div class='container'>";
+?>
 <?php foreach ($characters as $character) : ?>
 <?php
-   echo"<p class='character' id='character".$id."'> ".$character->name." </p>";
+   if ($id % 3 ==1)
+   {
+		echo "<div class='row'>";
+   }
+   echo "<div class ='col-sm-4'>";
+
+   // if the character is saved in the cookie, load a different HTML format
+   $isSelected=false;
+   foreach($items as $item)
+   {
+	   if($item->name == $character->name)
+	   {
+   		echo "<div class='characterBox panel panel-success'>";
+   		echo "<div class='panel-heading' id = 'characterName'>".$character->name."</div>";
+   		echo "<div class='panel-body'><a id = 'characterUrl' href='".$character->url."'>SWAPI Information</a></div>";
+   		echo "<div class='panel-footer'>Selected</div>";
+   		echo "</div>";
+		echo "</div>";
+		$isSelected = true;
+	   }
+   }
+
+   if(!$isSelected)
+   {
+	echo "<div class='characterBox panel panel-primary'>";
+	echo "<div class='panel-heading' id = 'characterName'>".$character->name."</div>";
+	echo "<div class='panel-body'><a id = 'characterUrl' href='".$character->url."'>SWAPI Information</a></div>";
+	echo "<div class='panel-footer'>Unselected</div>";
+	echo "</div>";
+ 	echo "</div>";
+   }
+
+   if ($id % 3 ==1)
+   {
+		echo "</div>";
+   }
    $id++;
 ?>
 <?php endforeach ?>
+<?php 
+	echo "</div>";
+?>
--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,10 +1,20 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Starwars Main</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script>
 	jQuery.noConflict();
 	jQuery(document).ready(function()
     {
         var pageNumber = <?= $page ?>;
+        var itemsSelected = [];
+        document.cookie="items="+JSON.stringify(itemsSelected);
 		jQuery("#characterBox").load("/home/view/" + pageNumber);
 	    jQuery("#prev").click(function()
 	    {
@@ -25,12 +35,38 @@
 		    jQuery("#characterBox").load("/home/view/" + pageNumber);
             jQuery("#pageCount").text("Page " + pageNumber);
 	    });
+        jQuery("body").on("click", "div.characterBox", function()
+        {
+            if (itemsSelected.length < 3)
+            {
+            jQuery(this).removeClass("panel-primary");
+            jQuery(this).addClass("panel-success");
+            jQuery(this).find(".panel-footer").text("Selected");
+            itemsSelected.push(
+                {
+                    name: jQuery(this).find("#characterName").text(),
+                    url: jQuery(this).find("#characterUrl").attr("href"),
+                }
+            );
+            document.cookie="items="+JSON.stringify(itemsSelected);
+            }
+            else
+            {
+                alert("You can only select 3 items.");
+            }
+        });
 	});
 </script>
+</head>
+<body>
 <div id="characterBox">
 </div>
-<div class ="navbar">
-<button id="prev">Previous page</button>
-<p id="pageCount">Page 1</p>
-<button id="next">Next page</button>
-</div>
+<nav class ="navbar navbar-default">
+<ul class="pager">
+<li class ='previous'><button id="prev" class="btn btn-primary">Previous page</button></li>
+<li class='active'><p id="pageCount">Page 1</p></li>
+<li class ='next'><button id="next" class="btn btn-primary">Next page</button></li>
+</ul>
+</nav>
+</body>
+</html>
